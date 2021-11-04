753d1b3441f5ac5942e8f21f6b6b87b16d71ab08
StéphaneCash <kikonistephane@gmail.com>
2021-11-04 05:45:19 +0100

    Arrangement Total

--- app/Http/Controllers/SpecialistesController.php
+++ app/Http/Controllers/SpecialistesController.php
@@ -3,29 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Specialiste;
 
 class SpecialistesController extends Controller
 {
     public function index()
     {
-        $specialiste = Specialiste::with('specialte')->get();
+        $specialiste = DB::select('SELECT * FROM specialistes, specialites, specialistesSpecialites
+             WHERE specialistesSpecialites.id_specialiste =  specialistes.id_specialiste 
+             AND specialites.id_specialite = specialistesSpecialites.id_specialite');
         return response()->json($specialiste);
     }
 
     public function store(Request $request)
     {
-        $specialiste = new Specialiste;
-        $specialiste->nom = $request->nom;
-        $specialiste->postnom = $request->postnom;
-        $specialiste->prenom = $request->prenom;
-        $specialiste->adresse = $request->adresse;
-        $specialiste->description = $request->description;
-        $specialiste->telephone = $request->telephone;
-        $specialiste->image = $request->image;
-        $specialiste->specialite_id = $request->specialite_id;
 
-        $specialiste->save();
+        $nom_specialiste = $request->nom_specialiste;
+        $postnom_specialiste = $request->postnom_specialiste;
+        $prenom_specialiste = $request->prenom_specialiste;
+        $telephone_specialiste = $request->telephone;
+
+        $nom_specialite = $request->nom_specialite;
+        $description_specialite = $request->description_specialite;
+
+        $specialiste = DB::insert("INSERT INTO specialistes 
+            (nom_specialiste, postnom_specialiste, prenom_specialiste, telephone) 
+            VALUES('$nom_specialiste', '$postnom_specialiste', '$prenom_specialiste', '$telephone_specialiste')");
+
+        $id_specialiste = DB::table('specialistes')->max('id_specialiste');
+
+        DB::insert("INSERT INTO specialites (nom_specialite, description_specialite)
+                VALUES('$nom_specialite', '$description_specialite')");
+
+        $id_specialite = DB::table('specialites')->max('id_specialite');
+
+        DB::insert("INSERT INTO specialistesSpecialites(id_specialiste, id_specialite) 
+            VALUES('$id_specialiste', '$id_specialite')");
+
         return response()->json($specialiste);
     }
 
--- app/Http/Controllers/SpecialitesController.php
+++ app/Http/Controllers/SpecialitesController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Specialite;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SpecialitesController extends Controller
 {
     public function index()
     {
-        $specialites = Specialite::with('specialistes')->get();
+        $specialites = DB::select('SELECT * FROM specialites, specialistes, specialistesSpecialites
+             WHERE specialistesSpecialites.id_specialite =  specialites.id_specialite 
+             AND specialistes.id_specialiste = specialistesSpecialites.id_specialiste');
         return response()->json($specialites);
     }
 
@@ -39,7 +42,8 @@ class SpecialitesController extends Controller
         return response()->json($specialite);
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
         $specialite = Specialite::findOrFail($id);
         $specialite->delete();
         return response()->json($specialite);
